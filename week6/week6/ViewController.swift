b878e85d3563476c247c88d7ab933b1e84f193fb
ALEX RUTH <aruth@10-150-44-13.dhcp-m.deakin.edu.au>
2017-04-20 10:32:34 +1000

    message

---
+++ week6/week6/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  week6
+//
+//  Created by ALEX RUTH on 20/04/2017.
+//  Copyright © 2017 ALEX RUTH. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
